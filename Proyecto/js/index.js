da381103e3057dcadd498f936f64ae3c11244a68
Anthony-M7 <135043556+Anthony-M7@users.noreply.github.com>
2023-07-24 11:37:49 -0400

    Arreglando Area de Categorias

--- Proyecto/js/index.js
+++ Proyecto/js/index.js
@@ -4,8 +4,10 @@ document
     var menu = document.getElementById("menu-hamburguesa");
     var boton = document.getElementById("boton-hamburguesa");
     var icono = document.querySelector("#boton-hamburguesa span");
+    var switchDark = document.querySelector("#switch");
 
     menu.classList.toggle("mostrando");
     boton.classList.toggle("mostrando");
     icono.classList.toggle("rotar");
+    switchDark.classList.toggle("dark");
   });
