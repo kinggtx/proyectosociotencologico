2f2651cda941a1a40049a55058eb551d5a23347c
Anthony-M7 <135043556+Anthony-M7@users.noreply.github.com>
2023-07-24 11:40:25 -0400

    Otra Vez

--- Proyecto/js/index.js
+++ Proyecto/js/index.js
@@ -4,10 +4,8 @@ document
     var menu = document.getElementById("menu-hamburguesa");
     var boton = document.getElementById("boton-hamburguesa");
     var icono = document.querySelector("#boton-hamburguesa span");
-    var switchDark = document.querySelector("#switch");
 
     menu.classList.toggle("mostrando");
     boton.classList.toggle("mostrando");
     icono.classList.toggle("rotar");
-    switchDark.classList.toggle("dark");
   });
