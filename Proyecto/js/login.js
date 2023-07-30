d7a2ddbd3b2230461575650f86373137ed46ec62
Eber_C <ghack7756@gmail.com>
2023-07-30 16:37:12 -0400

    eliminando cosas inecesarias

--- Proyecto/js/login.js
+++ Proyecto/js/login.js
@@ -1,23 +1,56 @@
+//Register
+function register() {
+  let email = document.correo;
+  let password = document.contrasena
+
+  localStorage.setItem("email", email);
+  localStorage.setItem("password", password);
+}
+
+function login() {
+  let email = document.login.email.value;
+  let password = document.login.password.value;
+
+  let storedEmail = localStorage.getItem("email");
+  let storedPassword = localStorage.getItem("password");
+
+  if (email === storedEmail && password === storedPassword) {
+    window.location = "../html/index.html";
+    console.log("registro exitoso");
+  } else {
+    console.log("USUARIO NO REGISTRADO");
+  }
+}
+
+// Login
 let flag = false;
 
-function Login() {
-  let correo = document.login.correo.value;
-  let password = document.login.contraseña.value;
+ function Login() {
+   let correo = document.login.correo.value;
+   let password = document.login.contrasena.value;
 
-  const user1 = {
-    email: "anthonymorag10@gmail.com",
-    password: "12345678",
-  };
+   const user1 = {
+     email: "anthonymorag10@gmail.com",
+     password: "12345678",
+   };
 
   flag = false;
   if (correo === user1.email && password === user1.password) {
-    flag = true;
+    flag = true
   }
 
   if (flag === true) {
-    // window.location = "../html/index.html";
+    window.location = "../html/index.html";
     console.log("Login EXITOSO!!!");
-  } else {
-    console.log("ERROR !!!!");
+   
   }
+  else if (email === storedEmail && password === storedPassword) {
+    window.location = "../html/index.html";
+    console.log("registro exitoso");
+  }
+  
+  else {
+    console.log("ERROR !!!!");
+   }
 }
+
