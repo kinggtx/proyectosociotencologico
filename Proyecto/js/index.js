e747d34ec611810d4852825eca1810bf91866706
Eber_C <ghack7756@gmail.com>
2023-07-30 21:59:39 -0400

    corrigiendo errores del modo dark

--- Proyecto/js/index.js
+++ Proyecto/js/index.js
@@ -10,24 +10,24 @@ document
     icono.classList.toggle("rotar");
   });
 
-//modo dark
-const switchButton = document.getElementById("switch");
-const workContainer = document.getElementById("work");
-
-// Recuperar la preferencia del usuario del almacenamiento local
-const darkMode = localStorage.getItem("darkMode");
-if (darkMode === "enabled") {
-  document.body.classList.add("dark-mode");
-  switchButton.checked = true;
-}
-// Agregar un escuchador de eventos al botón de cambio
-switchButton.addEventListener("change", () => {
-  // Alternar las clases en los elementos body y switchButton
-  document.body.classList.toggle("dark-mode");
-  // Guardar la preferencia del usuario en el almacenamiento local
-  if (document.body.classList.contains("dark-mode")) {
-    localStorage.setItem("darkMode", "enabled");
-  } else {
-    localStorage.removeItem("darkMode");
+  //modo dark
+  const switchButton = document.getElementById("switch");
+  const workContainer = document.getElementById("work");
+  
+  // Recuperar la preferencia del usuario del almacenamiento local
+  const darkMode = localStorage.getItem("darkMode");
+  if (darkMode === "enabled") {
+    document.body.classList.add("dark-mode");
+    switchButton.checked = true;
   }
-});
+  // Agregar un escuchador de eventos al botón de cambio
+  switchButton.addEventListener("change", () => {
+    // Alternar las clases en los elementos body y switchButton
+    document.body.classList.toggle("dark-mode");
+    // Guardar la preferencia del usuario en el almacenamiento local
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      localStorage.removeItem("darkMode");
+    }
+  });
